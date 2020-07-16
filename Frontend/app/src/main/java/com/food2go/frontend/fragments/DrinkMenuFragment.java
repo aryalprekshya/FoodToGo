3bf8bdc0d787fd1d3f101ba996998c4f316cc140
aryalprekshya <aryal.prekshyaa@gmail.com>
2020-07-16 00:44:52 -0400

    Appetizer, Meal and Drink menu created

---
+++ Frontend/app/src/main/java/com/food2go/frontend/fragments/DrinkMenuFragment.java
@@ -0,0 +1,63 @@
+package com.food2go.frontend.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.food2go.frontend.R;
+import com.food2go.frontend.adapter.RecyclerViewAdapter;
+import com.food2go.frontend.models.Menu;
+
+import java.util.ArrayList;
+
+public class DrinkMenuFragment extends Fragment {
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerViewAdapter recyclerViewAdapter;
+
+    public DrinkMenuFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        layoutManager= new GridLayoutManager(getContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerViewAdapter= new RecyclerViewAdapter(createDummyMenu());
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setHasFixedSize(true);
+
+        return view;
+    }
+    private ArrayList<Menu> createDummyMenu() {
+        Menu menu = new Menu("Category", "Chicken Popcorn", R.drawable.chickenpopcorn);
+        Menu menu1 = new Menu("Category", "Pan Fried Calamary", R.drawable.calamary);
+        Menu menu2 = new Menu("Category", "Stuffed Mushroom", R.drawable.mushroom);
+        Menu menu3 = new Menu("Category", "Fries", R.drawable.fries);
+
+        ArrayList<Menu> menuList = new ArrayList<Menu>();
+        menuList.add(menu);
+        menuList.add(menu1);
+        menuList.add(menu2);
+        menuList.add(menu3);
+
+        return menuList;
+    }
+}
